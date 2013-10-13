4f013bcf63efb4b2a6e2106fba4863e114701238
Xu FaSheng <fasheng.xu@gmail.com>
2013-10-13 11:48:07 +0800

    Update README and comment

--- dired-toggle.el
+++ dired-toggle.el
@@ -1,11 +1,10 @@
 ;;; dired-toggle.el --- provide a simple way to toggle dired buffer for current directory
 ;;
 ;; Copyright (C) 2013, Xu FaSheng
-;; Version: 20131012.2043
-;; X-Original-Version: 0.1
 ;;
 ;; Author: Xu FaSheng <fasheng.xu@gmail.com>
 ;; Maintainer: Xu FaSheng
+;; Version: 0.1
 ;; Package-Requires: ((dired "0"))
 ;; URL: https://github.com/fasheng/dired_toggle
 ;; Keywords: dired, toggle
@@ -32,38 +31,40 @@
 ;;
 ;;; Commentary:
 ;;
-;;  Description:
-;;  `dired-toggle' provide an simple way to toggle dired buffer for current
-;;  directory, and the target buffer is specified so that it will be divided
-;;  from other dired buffers, for example the dired buffer only shown
-;;  in a special buffer which could be edit through the variable
-;;  `dired-toggle-buffer-name', and it will include a minor mode named
-;;  `dired-toggle-mode' to ensure all the actions(such as change directory
-;;  and open a selected file) under the dired buffer will not influence
-;;  other dired buffers and works as we expected.
+;;; Description:
+;; `dired-toggle' provide an simple way to toggle dired buffer for current
+;; directory, and the target buffer is specified so that it will be divided
+;; from other dired buffers, for example the dired buffer only shown
+;; in a special buffer which could be edit through the variable
+;; `dired-toggle-buffer-name', and it will include a minor mode named
+;; `dired-toggle-mode' to ensure all the actions(such as change directory
+;; and open a selected file) under the dired buffer will not influence
+;; other dired buffers and works as we expected.
 ;;
-;;  You could custom the toggled window's size and side through variable
-;;  `dired-toggle-window-size' and `dired-toggle-window-side'.
+;; You could custom the toggled window's size and side through variable
+;; `dired-toggle-window-size' and `dired-toggle-window-side'.
 ;;
-;;  Usage:
-;;  Just add the following to your .emacs:
+;; site: https://github.com/fasheng/dired-toggle
 ;;
-;;  (global-set-key (kbd "<f5>") 'dired-toggle)
+;;; Usage:
+;; Just add the following to your .emacs:
 ;;
-;;  You could also custom functions after `dired-toggle-mode' enabled,
-;;  for example enable `visual-line-mode' for our narrow dired buffer:
+;; (global-set-key (kbd "<f5>") 'dired-toggle)
 ;;
-;;  (add-hook 'dired-toggle-mode-hook
-;;            (lambda () (interactive)
-;;              (visual-line-mode 1)
-;;              (setq-local visual-line-fringe-indicators '(nil right-curly-arrow))
-;;              (setq-local word-wrap nil)))
+;; You could also custom functions after `dired-toggle-mode' enabled,
+;; for example enable `visual-line-mode' for our narrow dired buffer:
 ;;
-;;  Default key-bindings:
-;;  | "q"       | dired-toggle-action-quit         |
-;;  | "RET"     | dired-toggle-action-find-file    |
-;;  | "^"       | dired-toggle-action-up-directory |
-;;  | "C-c C-u" | dired-toggle-action-up-directory |
+;; (add-hook 'dired-toggle-mode-hook
+;;           (lambda () (interactive)
+;;             (visual-line-mode 1)
+;;             (setq-local visual-line-fringe-indicators '(nil right-curly-arrow))
+;;             (setq-local word-wrap nil)))
+;;
+;;; Default key-bindings:
+;; | "q"       | dired-toggle-action-quit         |
+;; | "RET"     | dired-toggle-action-find-file    |
+;; | "^"       | dired-toggle-action-up-directory |
+;; | "C-c C-u" | dired-toggle-action-up-directory |
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
